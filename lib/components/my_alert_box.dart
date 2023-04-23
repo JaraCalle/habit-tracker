363b74fef70735a277b97d1898d742f104d9db8d
Juan Jara <jaracalle2041@gmail.com>
2023-04-23 15:40:53 -0500

    Funcionalidades de agregar, remover y editar habitos. Completamente funcional.

---
+++ lib/components/my_alert_box.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyAlertBox extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const MyAlertBox({super.key, required this.controller, required this.onSave, required this.onCancel});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      content: TextField(
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        decoration: const InputDecoration(
+          enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        ),
+      ),
+      actions: [
+        MaterialButton(
+          onPressed: onSave,
+          child: Text(
+              "Guardar",
+              style: TextStyle(color: Colors.white),
+          ),
+          color: Colors.black,
+        ),
+        MaterialButton(
+          onPressed: onCancel,
+          child: Text(
+            "Cancelar",
+            style: TextStyle(color: Colors.white),
+          ),
+          color: Colors.grey,
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:habit_tracker/components/habit_tile.dart';
+import 'package:habit_tracker/components/my_fab.dart';
+import 'package:habit_tracker/components/my_alert_box.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,13 +10,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Lista de habitos
+  List todaysHabitList = [
+    // [habitName, habitCompleted]
+    ["Leer", false],
+    ["Salir a trotar", false],
+  ];
+
+  // Combrobación para el checkbox
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      todaysHabitList[index][1] = value;
+    });
+  }
+
+  // Crear nuevo habito
+  final _newHabitNameController = TextEditingController();
+  void createNewHabit(){
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          onCancel: cancelarDialogBox,
+          onSave: GuardarNuevoHabito,
+        );
+      },
+    );
+  }
+
+  // Guardar nuevo habito
+  void GuardarNuevoHabito() {
+    // Agregar nuevo habito a la lista
+    setState(() {
+      todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+    // Limpiar el textfield
+    _newHabitNameController.clear();
+    // Eliminar dialog box
+    Navigator.of(context).pop();
+  }
+
+  // Cancelar nuevo habito
+  void cancelarDialogBox(){
+    // Limpiar el textfield
+    _newHabitNameController.clear();
+    // Eliminar dialog box
+    Navigator.of(context).pop();
+  }
+
+  // Abrir la opción para editar los habitos
+  void openHabitsSettings(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          onSave: () => guardarExistingHabit(index),
+          onCancel: cancelarDialogBox,
+        );
+    },);
+  }
+
+  void guardarExistingHabit(int index) {
+    setState(() {
+      todaysHabitList[index][0] = _newHabitNameController.text;
+    });
+  }
+
+  void borrarHabito(int index) {
+    setState(() {
+      todaysHabitList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: ListView(
-        children: [
-        ],
+      floatingActionButton: MyFloatingActionButton(onPressed: createNewHabit),
+      body: ListView.builder(
+        itemCount: todaysHabitList.length,
+        itemBuilder: (context, index) {
+          return HabitTile(
+            habitName: todaysHabitList[index][0],
+            habitCompleted: todaysHabitList[index][1],
+            onChanged: (value) => checkBoxTapped(value, index),
+            settingsTapped: (context) => openHabitsSettings(index),
+            deleteTapped: (context) => borrarHabito(index),
+          );
+        }
       ),
     );
   }
